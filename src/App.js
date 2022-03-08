3395a71f039ed8f97a46d4cb48f3956364d11b57
Lotus 98T <rpitty@live.com>
2022-03-08 21:06:25 +1000

    API fetch to display countries on home page.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import Navbar from "./Navbar";
 import Search from "./Search";
 import Countries from "./Countries";
 import Detail from "./Detail";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      // https://restcountries.com/#api-endpoints-v2
+      const response = await fetch(`https://restcountries.com/v2/all`);
+      const data = await response.json();
+      setCountries(data);
+    };
+
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
       <Search />
-      <Countries />
+      <Countries countries={ countries } />
     </div>
   );
 }
--- src/Countries.js
+++ src/Countries.js
@@ -1,21 +1,30 @@
 import "./css/Countries.css";
 
-const Countries = () => {
+const Countries = ({ countries }) => {
+    // If still loading countries data from API fetch
+    if (countries.length === 0) return <h2 style={ {marginLeft: `5rem`} }>Loading...</h2>
+
     return (
         <div className="Countries">
-            <div className="country-container">
-                <div className="country-flag">
-                    <p>Flag.png</p>
-                </div>
-                <div className="country-stats">
-                    <h2>Germany</h2>
-                    <ul>
-                        <li>Population: 81,770,900</li>
-                        <li>Region: Europe</li>
-                        <li>Capital: Berlin</li>
-                    </ul>
-                </div>
-            </div>
+
+            {
+                countries.map((country) => (
+                    <div className="country-container" key={ country.numericCode }>
+                        <div className="country-flag">
+                            <img src={ country.flag } alt="country-flag" />
+                        </div>
+                        <div className="country-stats">
+                            <h2>{ country.name }</h2>
+                            <ul>
+                                <li><span>Population:</span> { country.population }</li>
+                                <li><span>Region:</span> { country.region }</li>
+                                <li><span>Capital:</span> { country.capital }</li>
+                            </ul>
+                        </div>
+                    </div>
+                ))
+            }
+            
         </div>
     );
 }
